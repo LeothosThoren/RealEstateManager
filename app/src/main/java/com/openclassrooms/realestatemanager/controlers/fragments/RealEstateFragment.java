e26ada7f9308d187182bf198bbe76c43522978bd
leothos <soussou.med@live.fr>
2018-08-22 10:55:20 +0200

    Configure and display recylerview

--- app/src/main/java/com/openclassrooms/realestatemanager/controlers/fragments/RealEstateFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controlers/fragments/RealEstateFragment.java
@@ -5,21 +5,40 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.adapters.ImmovableAdapter;
+import com.openclassrooms.realestatemanager.models.Immovable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RealEstateFragment extends Fragment implements View.OnClickListener{
+public class RealEstateFragment extends Fragment implements View.OnClickListener {
 
-    //2 - Declare callback
+    // WIDGET
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    //DATA
+    private List<Immovable> mImmovableList;
+    //VAR
+    private ImmovableAdapter mAdapter;
+
+    // Declare callback
     private OnButtonClickedListener mCallback;
 
-    // 1 - Declare our interface that will be implemented by any container activity
+    // Declare our interface that will be implemented by any container activity
     public interface OnButtonClickedListener {
         public void onButtonClicked(View view);
     }
@@ -34,35 +53,58 @@ public class RealEstateFragment extends Fragment implements View.OnClickListener
                              Bundle savedInstanceState) {
         // Inflate the recycler_view_item_layout for this fragment
         View view = inflater.inflate(R.layout.fragment_real_estate, container, false);
+        ButterKnife.bind(this, view);
+        //Handle click
         view.findViewById(R.id.fragment_main_button).setOnClickListener(this);
+        // Call for new methods
+        this.configureRecyclerView();
         return view;
     }
 
+    // -------------------------------------------------------------------------------------------//
+    //                                    CONFIGURATION                                           //
+    // -------------------------------------------------------------------------------------------//
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        // 4 - Call the method that creating callback after being attached to parent activity
-        this.createCallbackToParentActivity();
+    // RecyclerView
+    private void configureRecyclerView() {
+        this.mImmovableList = new ArrayList<>();
+        this.mAdapter = new ImmovableAdapter(this.mImmovableList, Glide.with(this));
+        this.mRecyclerView.setAdapter(this.mAdapter);
+        this.mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
+
+    // Create callback to parent activity
+    private void createCallbackToParentActivity() {
+        try {
+            //Parent activity will automatically subscribe to callback
+            mCallback = (OnButtonClickedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(e.toString() + " must implement OnButtonClickedListener");
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------//
+    //                                       ACTION                                               //
+    // -------------------------------------------------------------------------------------------//
 
     @Override
     public void onClick(View view) {
         mCallback.onButtonClicked(view);
     }
 
-    // --------------
-    // FRAGMENT SUPPORT
-    // --------------
+    // -------------------------------------------------------------------------------------------//
+    //                                         UI                                                 //
+    // -------------------------------------------------------------------------------------------//
 
-    // 3 - Create callback to parent activity
-    private void createCallbackToParentActivity(){
-        try {
-            //Parent activity will automatically subscribe to callback
-            mCallback = (OnButtonClickedListener) getActivity();
-        } catch (ClassCastException e) {
-            throw new ClassCastException(e.toString()+ " must implement OnButtonClickedListener");
-        }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
     }
+
+
+
+
 }
